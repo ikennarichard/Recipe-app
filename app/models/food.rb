857f93ed3ff3f955594e57fc47b95a476cb8e682
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-01 04:20:59 +0100

    Add food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+    belongs_to :user, foreign_key: :user_id
+
+    validates :name, presence: true, uniqueness: true, length: { maximum: 250 }
+    validates :measurement_unit, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
