7c2a6479003a0eae6861ad1cd937d0b386cab0c5
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-01 09:59:01 -1100

    Update recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,15 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Recipe was successfully deleted.'
+  end
 end
