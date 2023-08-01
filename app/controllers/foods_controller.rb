e1e0a467cb816fa9679d223eebc24104d09ccb3b
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-01 12:52:20 +0100

    Add form to add new food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,27 @@
 class FoodsController < ApplicationController
+  def index
+    # @user = User.find(params[:user_id])
+    # @food = Food.where(user_id: @user.id)
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      flash[:notice] = 'Food created successfully!'
+      redirect_to foods_path(current_user)
+    else
+      flash[:alert] = "Couldn't create food!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root "users#index"
   
   resources :users
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
 end
