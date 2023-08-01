c14704ecd233c83165edd8ccda05abd435e1817b
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-01 11:52:44 -1100

    Update sign out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  # root "foods#index"
+  root "recipes#index"
+  get '/users/:id/sign_out', to: 'application#sign_out_user', as: 'sign_out_user'
   
   # resources :foods
   resources :recipes
