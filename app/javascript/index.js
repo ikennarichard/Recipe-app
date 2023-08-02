2027ac38ca434c1c70bbb1e8fcfc8785ccc79900
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-02 00:59:38 -1100

    Implement toggle click

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,7 +1,18 @@
 function slideUp() {
   const notice = document.querySelector('.notice');
   notice.classList.add('slide-up');
-  console.log('Thanks')
 }
 
-setTimeout(slideUp, 5000);
+
+function togglePublicStatus() {
+  const publicStatusElement = document.querySelector('.toggle-btn');
+  const toggler = document.querySelector('.toggle-btn-toggle');
+
+    publicStatusElement.classList.toggle('public');
+    toggler.click()
+    setTimeout(slideUp, 5000);
+};
+
+
+
+
