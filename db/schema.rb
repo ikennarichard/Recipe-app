738a9429e5a8df4be3d4123ab750f2c95fd440d6
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-02 01:05:48 -1100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_025633) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_095302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_025633) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.float "preparation_time", default: 0.0
@@ -35,14 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_025633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
-  create_table "recipes_foods", id: false, force: :cascade do |t|
-    t.bigint "recipe_id", null: false
-    t.bigint "food_id", null: false
-    t.integer "quantity"
-    t.index ["food_id", "recipe_id"], name: "index_recipes_foods_on_food_id_and_recipe_id"
-    t.index ["recipe_id", "food_id"], name: "index_recipes_foods_on_recipe_id_and_food_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,5 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_025633) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
