c5d77ca69c77d69348a5e0a72b4eca5ea318cb50
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-02 00:58:08 -1100

    Update recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   load_and_authorize_resource
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy, :toggle_public_status]
 
   def index
     @recipes = Recipe.all
@@ -8,6 +9,25 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = current_user.recipes.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render 'errors/not_found', status: :not_found
+  end
+
+  def new 
+    @recipe = Recipe.new
+  end
+  
+  def toggle_public_status
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.toggle_public_status
+
+    if @recipe.public?
+      redirect_to recipe_path(@recipe)
+      flash[:notice] = 'Recipe is set to public'
+    else
+      redirect_to recipe_path(@recipe)
+      flash[:notice] = 'Recipe is set to private'
+    end
   end
 
   def destroy
@@ -15,5 +35,11 @@ class RecipesController < ApplicationController
     @recipe.destroy
     redirect_to recipes_path
     flash[:notice] = 'Recipe was successfully deleted.'
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
