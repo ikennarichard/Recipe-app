d2a4facb16c7ab4ab4d2d1eb527e30a872d78fc6
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-02 01:02:15 -1100

    Update app models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
+  has_many :recipe_foods, dependent: :destroy
+  has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 250 }
   validates :measurement_unit, presence: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :foods, join_table: :recipes_foods
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
+
+  def toggle_public_status
+    self.public = !public
+    save
+  end
 end
