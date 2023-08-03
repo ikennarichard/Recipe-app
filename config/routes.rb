251b9fb95abe7e44ea33d7d5b937a09d712df652
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-02 14:23:33 -1100

    Update homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root "recipes#index"
+  root "foods#index"
   get '/users/:id/sign_out', to: 'application#sign_out_user', as: 'sign_out_user'
   
   resources :users
