609e44be1e7271e80b57e63b90045130185ffe89
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-03 15:08:16 -1100

    Fix linters

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -4,8 +4,8 @@ class ShoppingListController < ApplicationController
 
   def index
     @recipe_foods = RecipeFood.includes(recipe: :user, food: :user)
-    .where(users: { id: current_user.id })
-    .where('recipe_foods.quantity > foods.quantity')
+      .where(users: { id: current_user.id })
+      .where('recipe_foods.quantity > foods.quantity')
 
     @total_food_items = @recipe_foods.sum { |item| item.quantity - item.food.quantity }
     @total_price = @recipe_foods.sum { |item| (item.quantity - item.food.quantity) * item.food.price }
