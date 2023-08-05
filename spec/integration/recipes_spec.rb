0e12d3f382868e22418c78e30f572763da060f48
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-04 15:23:30 -1100

    Fix issues with spec files

--- spec/integration/recipes_spec.rb
+++ spec/integration/recipes_spec.rb
@@ -52,4 +52,30 @@ RSpec.describe 'Recipe Index', type: :feature do
     expect(current_path).to eq(recipes_path)
     expect(page).not_to have_content(recipe)
   end
+
+  scenario 'should display recipe details' do
+    user = create(:user)
+    recipe = create(:recipe, user:)
+
+    visit recipe_path(recipe)
+
+    expect(page).to have_content(recipe.name)
+    expect(page).to have_content('Preparation Time')
+    expect(page).to have_content('Cooking Time')
+    expect(page).to have_content('Public:')
+  end
+
+  scenario 'should redirect to add ingredient page' do
+    login_as(user)
+
+    recipe = user.recipes.create(name: 'Fried rice', preparation_time: 1.0, cooking_time: 1.0,
+                                 description: 'Awesome rice')
+
+    visit recipe_path(recipe)
+
+    click_link 'Add Ingredient'
+    sleep 3
+
+    expect(current_path).to eq(new_recipe_recipe_food_path(recipe))
+  end
 end
